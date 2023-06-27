9480921394e9a3cefc3a2fc5a31c354c0f4716b4
Alvannar254 <lanton254k@gmail.com>
2023-06-27 22:31:14 +0300

    printf project

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+void print_number(int n, int *count);
+
+#endif /* PRINT_FUNCTIONS_H */
+
