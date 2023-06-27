1456b9cf9cbbfda4ee39b12ccbfa84d8dc3dd8c1
Alvannar254 <lanton254k@gmail.com>
2023-06-28 01:57:09 +0300

    task 2

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -7,16 +7,17 @@ nclude "main.h"
  */
 int main(void)
 {
+	int num = 12345;
+	unsigned int binary = 98;
+
 	_printf("Let's try to printf a simple sentence.\n");
 	_printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	_printf("Percent:[%%]\n");
 
-	int num = 12345;
 	_printf("Integer:[%d]\n", num);
 	_printf("Integer:[%i]\n", num);
 
-	unsigned int binary = 98;
 	_printf("Binary:[%b]\n", binary);
 
 	return (0);
