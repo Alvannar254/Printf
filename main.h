9a582f27827b45ee2376709394586c0ce991faa1
Alvannar254 <lanton254k@gmail.com>
2023-06-28 05:08:36 +0300

    corrected

--- main.h
+++ main.h
@@ -1,14 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+/* task 0 */
 int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_str(va_list args);
 int print_format(char format, va_list args);
+
 int _print_int(int n);
 int _print_binary(unsigned int n);
 int _print_uint(unsigned int n);
